babf037c6c4f46993a5d98bf26fd4db39f9195e9
Gustavo.Lopes <glopoh@gmail.com>
2022-08-26 16:07:11 -0300

    Revert "Revert "Revert "Revert "Revert "Fix Startup.cs"""""
    This reverts commit e7a2c5458d907ed0ff3c451254d302d576b3c8df.

--- Startup.cs
+++ Startup.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-<<<<<<< HEAD
 using Microsoft.AspNetCore.Localization;
-=======
->>>>>>> parent of ab04d1d (App locale, number and date formatting)
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+
 using MVC.Models;
 using MVC.Data;
 using MVC.Services;
+using System.Collections.Generic;
 
 namespace MVC
 {
@@ -49,6 +49,16 @@ namespace MVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { en-US }
+            };
+
+            app.UseRequestLocalization(localizationOptions);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
